c1cbc6a7cf1fb086da76df571d90207ef7016e7f
Vincent Brodin <vincent.brodin21@gmail.com>
2026-03-02 01:00:00 +0100

    Added rayon

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -1,6 +1,9 @@
 use std::path::{Path, PathBuf};
 
 use gtfs_structures::RawGtfs;
+use rayon::slice::ParallelSliceMut;
+
+use crate::models::{Stop, StopIdx};
 
 /// Builds the .gtfs file
 #[derive(Debug, Default, Clone)]
@@ -16,7 +19,11 @@ impl Builder {
     }
 
     pub fn build(&self) -> Result<Vec<u8>, gtfs_structures::Error> {
-        let _gtfs = RawGtfs::from_path(&self.path)?;
+        let gtfs = RawGtfs::from_path(&self.path)?;
+        let stops = gtfs.stops?;
+        let stop_ids: Vec<&str> = stops.iter().map(|stop| stop.id.as_ref()).collect();
+        let mut lookup_vec: Vec<_> = (0..stops.len()).map(|i| StopIdx(i as u32)).collect();
+        // lookup_vec.par_sort_unstable_by(|a, b| stop_ids[a.into()].cmp(stop_ids[b.into()]));
         Ok(vec![])
     }
 }
