8c71fcbf5fffb0a881073ce54195a5d936ad6675
Vincent Brodin <vincent.brodin21@gmail.com>
2026-03-02 14:51:39 +0100

    Moved building stops logic to build_stops func

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 use gtfs_structures::RawGtfs;
 use rayon::slice::ParallelSliceMut;
 
-use crate::models::{Coordinate, Opt, Sentinel, Slice, Stop, StopIdSlice, StopIdx};
+use crate::models::{Coordinate, Opt, Sentinel, Slice, Stop, StopIdSlice, StopIdx, StringSlice};
 
 /// Builds the .gtfs file
 #[derive(Debug, Default, Clone)]
@@ -21,11 +21,19 @@ impl Builder {
     pub fn build(&self) -> Result<Vec<u8>, gtfs_structures::Error> {
         let gtfs = RawGtfs::from_path(&self.path)?;
         let raw_stops = gtfs.stops?;
+        let _stops = self.build_stops(&raw_stops)?;
+        Ok(vec![])
+    }
+
+    fn build_stops(
+        &self,
+        raw_stops: &[gtfs_structures::Stop],
+    ) -> Result<Vec<Stop>, gtfs_structures::Error> {
+        // Recomended max length for a gtfs id is 36 characters
         let mut stop_ids = String::with_capacity(36 * raw_stops.len());
-        let stops: Vec<_> = raw_stops
+        let mut stops: Vec<_> = raw_stops
             .iter()
-            .enumerate()
-            .map(|(i, stop)| {
+            .map(|stop| {
                 let id = StopIdSlice {
                     start: stop_ids.len() as u32,
                     count: stop.id.len() as u32,
@@ -41,21 +49,26 @@ impl Builder {
                 Stop {
                     coordinate,
                     id,
-                    code: Default::default(),
-                    name: Default::default(),
-                    desc: Default::default(),
-                    idx: StopIdx(i as u32),
-                    parent_idx: Default::default(),
+                    code: Opt::new(StringSlice::NONE),
+                    name: Opt::new(StringSlice::NONE),
+                    desc: Opt::new(StringSlice::NONE),
+                    idx: StopIdx::NONE,
+                    parent_idx: Opt::new(StopIdx::NONE),
                 }
             })
             .collect();
         //TODO: ADD SORT STOPS LOGIC GOES HERE
+        // Set stop idx
+        stops
+            .iter_mut()
+            .enumerate()
+            .for_each(|(i, stop)| stop.idx = StopIdx(i as u32));
         let mut stop_id_lookup: Vec<_> = (0..stops.len()).map(|i| StopIdx(i as u32)).collect();
         stop_id_lookup.par_sort_unstable_by(|a, b| {
             let id_a = &stop_ids[stops[a.to_usize()].id.range()];
             let id_b = &stop_ids[stops[b.to_usize()].id.range()];
             id_a.cmp(id_b)
         });
-        Ok(vec![])
+        Ok(stops)
     }
 }
