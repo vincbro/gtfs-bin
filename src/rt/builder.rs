585e21b9b01b0c7407fce1910c801149be0dc450
vincbro <vincent.brodin21@gmail.com>
2026-04-17 20:30:28 +0200

    Added test and first valid iteration of RT

--- src/rt/builder.rs
+++ src/rt/builder.rs
@@ -1,4 +1,10 @@
-use crate::{consumer::Consumer, rt::Realtime};
+use gtfs_rt::trip_descriptor::ScheduleRelationship;
+
+use crate::{
+    consumer::Consumer,
+    models::{Coordinate, Delay, Opt},
+    rt::{Realtime, TripStatus, Vehicle},
+};
 
 pub struct RealtimeBuilder<'a> {
     consumer: &'a Consumer<'a>,
@@ -24,17 +30,61 @@ impl<'a> RealtimeBuilder<'a> {
     {
         for message in messages {
             for entity in message.entity.into_iter() {
-                if let Some(trip_update) = entity.trip_update {
-                    if let Some(trip) = self.consumer.trip_by_id(trip_update.trip.trip_id()) {
-                        for stop_time_update in trip_update.stop_time_update.into_iter() {}
+                if let Some(trip_update) = entity.trip_update
+                    && let Some(trip) = self.consumer.trip_by_id(trip_update.trip.trip_id())
+                {
+                    match trip_update.trip.schedule_relationship() {
+                        ScheduleRelationship::Scheduled => {
+                            realtime.trip_status[trip.idx.as_usize()] = TripStatus::Unchanged
+                        }
+                        ScheduleRelationship::Added => {
+                            realtime.trip_status[trip.idx.as_usize()] = TripStatus::Added
+                        }
+                        ScheduleRelationship::Unscheduled => {
+                            realtime.trip_status[trip.idx.as_usize()] = TripStatus::Unscheduled
+                        }
+                        ScheduleRelationship::Canceled => {
+                            realtime.trip_status[trip.idx.as_usize()] = TripStatus::Cancled
+                        }
+                        ScheduleRelationship::Deleted => {
+                            realtime.trip_status[trip.idx.as_usize()] = TripStatus::Deleted
+                        }
+                        _ => (),
+                    }
+                    let stop_times = self.consumer.stop_times_by_trip(trip.idx);
+                    for stop_time_update in trip_update.stop_time_update.into_iter() {
+                        if let Ok(idx) = stop_times.binary_search_by_key(
+                            &stop_time_update.stop_sequence.unwrap_or(u32::MAX),
+                            |st| st.sequence,
+                        ) {
+                            let idx = trip.stop_times.start as usize + idx;
+                            if let Some(departure) = stop_time_update.departure {
+                                realtime.stop_time_departure_delays[idx] =
+                                    Opt::new(Delay(departure.delay() as i16))
+                            }
+
+                            if let Some(arrival) = stop_time_update.arrival {
+                                realtime.stop_time_arrival_delays[idx] =
+                                    Opt::new(Delay(arrival.delay() as i16))
+                            }
+                        }
                     }
                 }
 
-                if let Some(vehicle) = entity.vehicle {}
+                if let Some(vehicle) = entity.vehicle
+                    && let Some(position) = vehicle.position
+                    && let Some(trip_descriptor) = vehicle.trip
+                    && let Some(trip) = self.consumer.trip_by_id(trip_descriptor.trip_id())
+                {
+                    realtime.trip_vehicles[trip.idx.as_usize()] = Some(Vehicle {
+                        position: Coordinate::new(
+                            position.latitude as f64,
+                            position.longitude as f64,
+                        ),
+                    })
+                }
 
-                if let Some(alert) = entity.alert {}
-
-                if let Some(trip_modifications) = entity.trip_modifications {}
+                // if let Some(trip_modifications) = entity.trip_modifications {}
             }
         }
     }
--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -4,19 +4,38 @@ pub use builder::*;
 
 use crate::{
     consumer::Consumer,
-    models::{Delay, Opt, Sentinel},
+    models::{Coordinate, Delay, Opt, Sentinel},
 };
 
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TripStatus {
+    #[default]
+    Unchanged,
+    Added,
+    Cancled,
+    Deleted,
+    Unscheduled,
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+pub struct Vehicle {
+    pub position: Coordinate,
+}
+
 pub struct Realtime {
-    stop_time_delays: Vec<Opt<Delay>>,
-    active_trips: Vec<bool>,
+    pub stop_time_departure_delays: Vec<Opt<Delay>>,
+    pub stop_time_arrival_delays: Vec<Opt<Delay>>,
+    pub trip_status: Vec<TripStatus>,
+    pub trip_vehicles: Vec<Option<Vehicle>>,
 }
 
 impl Realtime {
     pub fn new(consumer: &Consumer) -> Self {
         Self {
-            stop_time_delays: vec![Opt::new(Delay::NONE); consumer.stop_times.len()],
-            active_trips: vec![true; consumer.trips.len()],
+            stop_time_departure_delays: vec![Opt::new(Delay::NONE); consumer.stop_times.len()],
+            stop_time_arrival_delays: vec![Opt::new(Delay::NONE); consumer.stop_times.len()],
+            trip_status: vec![TripStatus::Unchanged; consumer.trips.len()],
+            trip_vehicles: vec![None; consumer.trips.len()],
         }
     }
 }
